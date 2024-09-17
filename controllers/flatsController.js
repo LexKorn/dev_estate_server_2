20e7ef644e4a3c86c3c0ab8c8053fdcc8ff4145b
LexKorn <fischer84@mail.ru>
2024-09-17 08:59:23 +0300

    fixed filter

--- controllers/flatsController.js
+++ controllers/flatsController.js
@@ -4,6 +4,17 @@ class FlatsController {
     async getAll(req, res) {
         try {
             let {limit, page, checkFirst, checkSecond, checkedValues, region, priceMin, priceMax, areaMin, areaMax, levelMin, levelMax} = req.query;
+           
+            checkFirst = checkFirst || false;
+            checkSecond = checkSecond || false;
+            checkedValues = checkedValues || [];
+            region = region || 0;
+            priceMin = priceMin || 750000;
+            priceMax = priceMax || 30000000;
+            areaMin = areaMin || 10;
+            areaMax = areaMax || 200;
+            levelMin = levelMin || 1;
+            levelMax = levelMax || 40;
             page = page || 1;
             limit = Number(limit) || 10;
             let offset = page * limit - limit;
@@ -12,9 +23,9 @@ class FlatsController {
                 if ((checkFirst && checkSecond) || (!checkFirst && !checkSecond) ) {
                     return flats;
                 } else if (checkFirst && !checkSecond) {
-                    return flats.filter(flat => flat.object_type !== 1);
+                    return flats.filter(flat => flat.object_type != 1);
                 } else if (!checkFirst && checkSecond) {
-                    return flats.filter(flat => flat.object_type === 1);
+                    return flats.filter(flat => flat.object_type == 1);
                 }
             }
 
@@ -30,21 +41,25 @@ class FlatsController {
                 if (!region) {
                     return flats;
                 } else {
-                    return flats.filter(flat => flat.region === region);
+                    return flats.filter(flat => flat.region == region);
                 }
             }
 
             function filterRanges(flats) {
                 let arr = [];
-                arr = flats.filter(flat => flat.price >= priceMin &price <= priceMax);
-                arr = arr.filter(flat => flat.area >= areaMin &area <= areaMax);
-                arr = arr.filter(flat => flat.level >= levelMin &level <= levelMax);
+                arr = flats.filter(flat => flat.price >= priceMin & flat.price <= priceMax);
+                arr = arr.filter(flat => flat.area >= areaMin & flat.area <= areaMax);
+                arr = arr.filter(flat => flat.level >= levelMin & flat.level <= levelMax);
                 return arr;
             }
 
 
-            const flats = await Flat.findAndCountAll({limit, offset});
-            return res.json(flats);
+            // const flats = await Flat.findAndCountAll({limit, offset});
+            const flats = await Flat.findAndCountAll();
+            const filteredFlats = filterRooms(filterRanges(filterObjectType(filterRigions(flats.rows))));
+
+            // return res.json(flats);
+            return res.json(filteredFlats);
         } catch(err) {
             res.status(400).json(err.message);
         }
